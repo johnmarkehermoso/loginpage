039160f10847a8d1492b3a081b39f00a9ad5d4ed
johnmarkehermoso <johnmarkehermoso@gmail.com>
2023-04-11 18:03:49 +0800

    initial upload

---
+++ login.sql
@@ -0,0 +1,80 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/stylecss.css"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Secular+One&display=swap" rel="stylesheet">
+    <!-- bootstrap css  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <!-- css animation bootstrap -->
+    <!-- <linkrel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/> -->
+    <!-- bootstrap css -->
+    <!-- bootstrap js -->
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <!-- bootstrap js -->
+    <link rel="stylesheet" href="style1.css"/>
+    <title>Login Page</title>
+</head>
+<body class="loginpage" style="background-color: #b2b2b2;">
+  <div class="container">
+    <section class="vh-80 gradient-custom">
+        <div class="container py-5 w-70">
+          <div class="row d-flex justify-content-center align-items-center h-70">
+            <div class="col col-xl-5">
+              <div class="card text-white" style="border-radius: 1rem;">
+                <div class="card-body p-3 text-center">
+      
+                  <div class="mb-md-5 mt-md-4 pb-5">
+      
+                    <div class="d-flex flex-column align-items-center">
+                      <img id="profile" class="photo" width="100px"
+                      height="100px" src="images/portfolio.jpg" alt=""/>
+                    </div>
+      
+                    <div class="form-group row">
+                      <label class="col-sm-3 col-form-label mt-5" for="enterEmail">Email</label>
+                      <div class="col-sm-9 mb-4">
+                        <input type="email" class="form-control mt-5" id="enterEmail" placeholder="Enter email address">
+                      </div> 
+                    </div>
+      
+                    <div class="form-group row">
+                      <label class="col-sm-3 col-form-label" for="enterPassword">Password</label>
+                      <div class="col-sm-9 mb-4">
+                        <input type="password" class="form-control" id="enterPassword" placeholder="Enter password">
+                      </div> 
+                    </div>
+      
+                    <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p>
+      
+                    <button class="btn btn-outline-dark btn-lg px-5" type="submit">Login</button>
+
+                    <div class="d-flex justify-content-center text-center mt-4 pt-1">
+                      <a href="#!" class="text-white"><i class="fab fa-facebook-f fa-lg"></i></a>
+                      <a href="#!" class="text-white"><i class="fab fa-twitter fa-lg mx-4 px-2"></i></a>
+                      <a href="#!" class="text-white"><i class="fab fa-google fa-lg"></i></a>
+                    </div>
+      
+                  </div>
+      
+                  <div>
+                    <p class="mb-0">Don't have an account? <a href="#!" class="text-white-50 fw-bold">Sign Up</a>
+                    </p>
+                  </div>
+                    <p> or connect with using</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+</body>
+</html>
